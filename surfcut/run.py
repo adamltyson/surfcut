f4825f1f972e3539e95c36021dc518b7583cf74d
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-02-29 19:53:33 +0100

    extract surface

--- surfcut/run.py
+++ surfcut/run.py
@@ -29,8 +29,30 @@ def main():
     print("Thresholding")
     binary = 1.0 * (filtered > args.threshold)
 
-    view(data, filtered, binary)
+    print("Detecting edges")
+    edges = edge_detect(binary)
+    view(data, filtered, binary, edges)
 
+
+def edge_detect(image):
+    # // Edge
+    # detect
+    # getDimensions(w, h, channels, slices, frames);
+    # print(slices);
+    # for (img=0; img < slices; img++){
+    #     print("Edge detect projection" + img + "/" + slices);
+    # slice = img+1;
+    # selectWindow(imgName);
+    # run("Z Project...", "stop=&slice projection=[Max Intensity]");
+    # }
+
+    # FIXME: why doesnt this work?
+    edges = np.zeros_like(image)
+    for idx, _ in enumerate(image):
+        if idx < len(image):
+            edges[idx, :, :] = np.max(image[0:idx+1], axis=0)
+
+    return edges
 
 if __name__ == "__main__":
     main()
