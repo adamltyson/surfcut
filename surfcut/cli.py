25547e72abe7a90b069fbb178412b9b8d0e51159
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-03-01 14:08:36 +0100

    Projection calculation

--- surfcut/cli.py
+++ surfcut/cli.py
@@ -3,6 +3,7 @@ from argparse import (
     ArgumentDefaultsHelpFormatter,
 )
 
+
 def surfcut_parser():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument(
@@ -10,6 +11,49 @@ def surfcut_parser():
         type=str,
         nargs=1,
         help="Path to the image to be analysed",
+    )
+    parser.add_argument(
+        "-g",
+        "--gauss_sigma",
+        dest="gauss_sigma",
+        type=int,
+        default=3,
+        help="Gaussian smoothing sigma width",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        dest="threshold",
+        type=int,
+        default=20,
+        help="Threshold for binarising the image",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--shift",
+        dest="shift",
+        type=int,
+        default=14,
+        help="Where relative to the surface of the sample should the "
+        "cut be made",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--depth",
+        dest="depth",
+        type=int,
+        default=4,
+        help="Thickness of the projection",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--no-viewer",
+        dest="no_viewer",
+        action="store_true",
+        help="Don't launch the viewer",
     )
 
     return parser
