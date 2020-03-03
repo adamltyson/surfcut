7310b724d1dd7fc8f2658ebfeb3e547b7ab3bc27
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-03-03 09:19:04 +0100

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 requirements = [
@@ -18,10 +18,10 @@ requirements = [
 
 setup(
     name="surfcut",
-    version="0.0.3rc0",
+    version="0.0.3rc1",
     description="Cutting confocal stacks at various depths relative to surface signal ",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     install_requires=requirements,
     python_requires=">=3.6",
     packages=find_packages(),
